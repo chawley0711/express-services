9c51cbeca92563a05cf69dad0d69de924a4c1015
Andrew <andrewsylvester117@gmail.com>
2018-02-07 10:04:39 -0700

    Added routing to the orders, service, and home page in the index.js.

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var pug = require('pug');
+var path = require('path');
+var config = require('./menu');
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname+'/views');
+app.use(express.static(path.join(__dirname+'/public')));
+
+app.get('/', function(req,res){
+    res.render('home', {
+        "title": "Home",
+        "config":config
+    })
+});
+
+app.get('/services', function(req,res){
+    res.render('services',{
+        "title": "Services"
+    })
+});
+
+app.get('/orders', function(req,res){
+    res.render('orders', {
+        "title": "Orders"
+    })
+});
+
+app.listen(3000);
