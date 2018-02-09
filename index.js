c3b2c64f6953a63d8ed546f29747e858b6370fde
Andrew <andrewsylvester117@gmail.com>
2018-02-09 10:02:40 -0700

    Worked on Css

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname+'/public')));
 
 app.get('/', function(req,res){
     res.render('home', {
-        "title": "Home",
+        "title": "Home"
     })
 });
 
