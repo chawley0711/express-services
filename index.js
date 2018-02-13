184423369f0818737de43fc226126908c6ce6d6c
Andrew <andrewsylvester117@gmail.com>
2018-02-13 01:36:48 -0700

    Made Css Changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname+'/public')));
 
 app.get('/', function(req,res){
     res.render('home', {
-        "title": "Home",
+        "title": "The Best Barbershop",
         "name": "The Best Barbershop",
         "city": "New Mexico"
     })
